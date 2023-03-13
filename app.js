96ac469cd697b7f3454b1e10d08bd8fd25c83d16
David Petryca <david@petryca.com>
2023-03-13 13:32:34 +0100

    update

--- app.js
+++ app.js
@@ -1,28 +1,6 @@
-const items = document.querySelectorAll('#grid > div');
-
-for (const item of items.values()) {
-    
-    new Waypoint.Inview({
-        element: item,
-        enter(direction) {
-            if (direction == 'down') {
-                item.classList.add('move-up');
-            }
-        },
-        exited(direction) {
-            if (direction == 'up') {
-                item.classList.remove('move-up');
-            }
-        }
-    });
-    
-}
 
 
-
-
-
-
-
-
-
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('x');
+    nav.classList.toggle('open');
+});
