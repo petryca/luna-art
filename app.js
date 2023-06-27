2faae55fa30513255ce6096d04da163b3c43bd9e
David Petryca <david@petryca.com>
2023-06-27 19:03:59 +0200

    update w2 tue

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ ham.addEventListener('click', () => {
 
 mapboxgl.accessToken = 'pk.eyJ1IjoicGV0cnljYSIsImEiOiJjazd6eGFxajEwOW5rM2RydW5rb3pzcmtiIn0.eFkGZsTPafVGw_E9bXI8aA';
 
+let gps = [14.439972261923986, 50.078594071059776];
+
 let map = new mapboxgl.Map({
     container: 'studio',
-    center: [14.439972261923986, 50.078594071059776],
+    center: gps,
     zoom: 15,
     style: 'mapbox://style/mapbox/dark-v10'
 });
@@ -19,7 +21,30 @@ map.addControl(new mapboxgl.NavigationControl());
 let elm = document.createElement('div');
 elm.className = 'pin';
 
+let html = '<div class="popup">My Studio is Here</div>';
+
+let popup = new mapboxgl.Popup().setHTML(html);
+
 new mapboxgl.Marker({
     element: elm,
     anchor: 'bottom'
-}).setLngLat([14.439972261923986, 50.078594071059776]).addTo(map);
+}).setLngLat(gps).addTo(map).setPopup(popup);
+
+form.addEventListener('submit', (evnt) => {
+    evnt.preventDefault();
+    console.log(email.value, sender.value, message.value);
+    form.reset();
+    form.classList.remove('validate');
+    response.style.display = 'flex';
+    setTimeout(() => {
+        response.style.display = 'none';
+    }, 5000);
+});
+
+btnsubmit.addEventListener('click', () => {
+    form.classList.add('validate');
+});
+
+btnreset.addEventListener('click', () => {
+    form.classList.remove('validate');
+});
